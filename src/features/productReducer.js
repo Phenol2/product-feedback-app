29ad37a4501eab086dd2683ae6e4adbfdd97df72
Phenol2 <91699783+Phenol2@users.noreply.github.com>
2023-02-04 00:54:17 +0100

    redux setup

---
+++ src/features/productReducer.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+let initialState = {
+
+}
+
+let productSlice = createSlice({
+  name: "product",
+  initialState,
+  reducers:{
+
+  },
+})
+
+export default productSlice
