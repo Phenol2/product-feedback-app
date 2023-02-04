dd603ffbc2bf675c23afbc5766740ac8900ae07f
Phenol2 <91699783+Phenol2@users.noreply.github.com>
2023-02-04 07:14:41 +0100

    styling the suggestion component

--- src/components/Product/Product.styled.js
+++ src/components/Product/Product.styled.js
@@ -3,10 +3,17 @@ import styled from "styled-components"
 export const Container = styled.section`
     width: 100%;
     display: flex;
+    gap: 20px;
+    background-color: white;
+    padding: 30px 15px;
+    margin-bottom: 20px;
+    border-radius: 10px;
 `
 
 export const Upvotes = styled.div`
-
+    font-size: 12px;
+    font-weight: bold;
+    color: #230261;
 `
 
 export const Details = styled.div`
